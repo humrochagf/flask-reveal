e5eb60a51359867b490eeafc8ecfd9c384ceeb06
Humberto Rocha <humrochagf@gmail.com>
2014-08-29 08:54:51 -0300

    adding coding

--- flask_reveal/config.py
+++ flask_reveal/config.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 REVEAL_META = {
     'title': 'The title',
     'author': 'Some Author',
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask_reveal.app import create_app
 
 
