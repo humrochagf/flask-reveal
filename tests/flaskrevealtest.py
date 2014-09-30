f93e67871fd0df9a17472d03d0c346e34d49c676
Humberto Rocha <humrochagf@gmail.com>
2014-09-30 12:01:03 -0300

    fix app incorrect reference

--- tests/flaskrevealtest.py
+++ tests/flaskrevealtest.py
@@ -10,7 +10,7 @@ class FlaskRevealTestCase(unittest.TestCase):
         app_config = FlaskReveal('flask_reveal')
 
         app_config.config['PRESENTATION_ROOT'] = os.path.abspath(os.path.join(__file__, '../exaple_slides'))
-        app_config.config['MEDIA_ROOT'] = os.path.join(self.app.config['PRESENTATION_ROOT'], 'img')
+        app_config.config['MEDIA_ROOT'] = os.path.join(app_config.config['PRESENTATION_ROOT'], 'img')
         app_config.config['TESTING'] = True
 
         self.app = app_config.test_client()
