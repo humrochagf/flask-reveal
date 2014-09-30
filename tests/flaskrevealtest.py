403d5627aac161a24ecb05a69ab75c349a8892e2
Humberto Rocha <humrochagf@gmail.com>
2014-09-30 11:10:56 -0300

    starting test bases

---
+++ tests/flaskrevealtest.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import os
+import unittest
+
+from flask_reveal.app import FlaskReveal
+
+
+class FlaskRevealTestCase(unittest.TestCase):
+    def setUp(self):
+        app_config = FlaskReveal('flask_reveal')
+
+        app_config.config['PRESENTATION_ROOT'] = os.path.abspath(os.path.join(__file__, '../exaple_slides'))
+        app_config.config['MEDIA_ROOT'] = os.path.join(self.app.config['PRESENTATION_ROOT'], 'img')
+        app_config.config['TESTING'] = True
+
+        self.app = app_config.test_client()
+
+
+if __name__ == '__main__':
+    unittest.main()
