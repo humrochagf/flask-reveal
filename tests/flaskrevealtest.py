50f8935b6ef5cfc233910351911d27d6471ef216
Humberto Rocha <humrochagf@gmail.com>
2014-10-01 10:12:53 -0300

    test load_markdown_slides

--- tests/flaskrevealtest.py
+++ tests/flaskrevealtest.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 import os
+from os import fdopen
+import shutil
 import unittest
+import tempfile
 
 from flask_reveal.app import FlaskReveal
+from flask_reveal.blueprints.utils import load_markdown_slides
 
 
 class FlaskRevealTestCase(unittest.TestCase):
@@ -25,6 +29,18 @@ class FlaskRevealTestCase(unittest.TestCase):
     def test_get_img(self):
         with self.app.get('/img/python.png') as response:
             self.assertEqual(response.status, '200 OK')
+
+    def test_slide_loading(self):
+        directory = tempfile.mkdtemp()
+        fd, file = tempfile.mkstemp('.md', dir=directory)
+        with fdopen(fd, 'w') as f:
+            f.write('test')
+
+        slides = load_markdown_slides(directory)
+
+        self.assertEqual(slides, ['test',])
+
+        shutil.rmtree(directory)
 
 
 if __name__ == '__main__':
