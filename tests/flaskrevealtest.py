4a48536415e8d5b99b6a1ef50910c8e5d175e430
Humberto Rocha <humrochagf@gmail.com>
2014-10-02 10:43:15 -0300

    refactoring tests for example_slides independency

--- tests/flaskrevealtest.py
+++ tests/flaskrevealtest.py
@@ -10,37 +10,49 @@ from flask_reveal.blueprints.utils import load_markdown_slides
 
 
 class FlaskRevealTestCase(unittest.TestCase):
-    def create_app(self):
-        presentation_root = os.path.abspath(os.path.join(os.path.basename(__file__), '../example_slides'))
+    slides = ['Slide1', 'Slide2', 'Slide3']
+
+    def create_presentation_structure(self, slides=None):
+        presentation_root = tempfile.mkdtemp()
+        media_root = tempfile.mkdtemp()
+        fd, img_file = tempfile.mkstemp('.jpg', dir=media_root)
+
+        os.close(fd)
+
+        for index, slide in enumerate(slides):
+            fd, _ = tempfile.mkstemp('.md', str(index), presentation_root)
+            with fdopen(fd, 'w') as file:
+                file.write(slide)
+
+        return presentation_root, media_root, os.path.basename(img_file)
+
+    def create_app(self, presentation_root, media_root):
         app_config = FlaskReveal('flask_reveal')
         app_config.config['PRESENTATION_ROOT'] = presentation_root
-        app_config.config['MEDIA_ROOT'] = os.path.join(presentation_root, 'img')
+        app_config.config['MEDIA_ROOT'] = media_root
         app_config.config['TESTING'] = True
 
         return app_config.test_client()
 
     def setUp(self):
-        self.app = self.create_app()
+        self.presentation_root, self.media_root, self.img_file = self.create_presentation_structure(self.slides)
+        self.app = self.create_app(self.presentation_root, self.media_root)
+
+    def tearDown(self):
+        shutil.rmtree(self.presentation_root)
 
     def test_presentation_view_status(self):
         with self.app.get('/') as response:
             self.assertEqual(response.status, '200 OK')
 
-    def test_get_img(self):
-        with self.app.get('/img/python.png') as response:
+    def test_get_img_view_status(self):
+        with self.app.get('/img/{0}'.format(self.img_file)) as response:
             self.assertEqual(response.status, '200 OK')
 
-    def test_slide_loading(self):
-        directory = tempfile.mkdtemp()
-        fd, file = tempfile.mkstemp('.md', dir=directory)
-        with fdopen(fd, 'w') as f:
-            f.write('test')
+    def test_load_markdown_slides(self):
+        slides = load_markdown_slides(self.presentation_root)
 
-        slides = load_markdown_slides(directory)
-
-        self.assertEqual(slides, ['test',])
-
-        shutil.rmtree(directory)
+        self.assertEqual(slides, self.slides)
 
 
 if __name__ == '__main__':
