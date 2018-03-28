91d6af7a0b4f603066f38ab42649255b4026aec6
Humberto Rocha <humrochagf@gmail.com>
2018-03-28 10:08:12 -0300

    improve setup

--- setup.py
+++ setup.py
@@ -1,49 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+'''flask-reveal setup file'''
 
 import os
 import re
 
 from setuptools import find_packages, setup
 
-try:
-    import pypandoc
-    readme = pypandoc.convert('README.md', 'rst')
-except (IOError, ImportError):
-    readme = ''
+with open('README.md', 'r') as readme_file:
+    README = readme_file.read()
 
 # package variables
-package = 'flask_reveal'
-requirements = [
+PACKAGE = 'flask_reveal'
+REQUIREMENTS = [
     'Flask>=0.12',
 ]
-test_requirements = [
-    'mock==2.0.0'
+TEST_REQUIREMENTS = [
+    'coverage==4.4.1',
+    'mock==2.0.0',
+    'nose==1.3.7',
+    'pylint-flask==0.5',
+    'pylint==1.7.1',
 ]
 
 # dynamic package info
-init_py = open(os.path.join(package, '__init__.py')).read()
-version = re.search(
-    "^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
-author = re.search(
-    "^__author__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
-email = re.search(
-    "^__email__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
+with open(os.path.join(PACKAGE, '__init__.py')) as init_file:
+    INIT = init_file.read()
+VERSION = re.search(
+    "^__version__ = ['\"]([^'\"]+)['\"]", INIT, re.MULTILINE).group(1)
+AUTHOR = re.search(
+    "^__author__ = ['\"]([^'\"]+)['\"]", INIT, re.MULTILINE).group(1)
+EMAIL = re.search(
+    "^__email__ = ['\"]([^'\"]+)['\"]", INIT, re.MULTILINE).group(1)
 
 setup(
     name='flask-reveal',
-    version=version,
+    version=VERSION,
     description='Make reveal.js presentations with Flask',
-    long_description=readme,
-    long_description_content_type='text/x-rst',
-    author=author,
-    author_email=email,
+    long_description=README,
+    long_description_content_type='text/markdown; charset=UTF-8',
+    author=AUTHOR,
+    author_email=EMAIL,
     url='https://github.com/humrochagf/flask-reveal',
     license='MIT',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=requirements,
+    install_requires=REQUIREMENTS,
     entry_points=dict(
         console_scripts=['flaskreveal=flask_reveal.tools.cli:cli_execute']),
     platforms='any',
@@ -65,5 +68,5 @@ setup(
         'Topic :: Text Processing :: Markup :: HTML',
     ],
     test_suite='tests',
-    tests_require=test_requirements
+    tests_require=TEST_REQUIREMENTS
 )
